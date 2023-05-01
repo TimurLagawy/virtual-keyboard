10651f865ea4e2827fad2000c2fa9cc2203f3c05
Timur Lagawy <timurtagirovich@mail.ru>
2023-05-01 17:02:10 +0200

    refactor: add some code script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57];
-document.onkeypress = function (event) {
-  //console.log(event);
+let keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8];
+/*document.onkeypress = function (event) {
+  console.log(event);
   keyboard.push(event.charCode);
   console.log(keyboard);
-};
+};*/
 
 function init() {
-  let out = "";
+  let out = " ";
   for (let i = 0; i < keyboard.length; i++) {
-    out +=
-      '<div class="keyboard-key">' + String.fromCharCode(keyboard[i]) + "<div>";
+    out += '<div class="k-key" >' + String.fromCharCode(keyboard[i]) + "</div>";
   }
   document.querySelector("#keyboard").innerHTML = out;
 }
